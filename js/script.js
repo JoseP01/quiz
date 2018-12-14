a6439873caf9b1c39ebd27f09aa898c4e603efbc
Jose Pelayo <s_josemaria.pelayomarin@ousd.org>
2018-12-14 02:27:00 +0000

    hi

--- js/script.js
+++ js/script.js
@@ -8,7 +8,48 @@ $(document).ready(function() {
         var q1Result = $("#question1").val();
         var q2Result = $("#question2").val();
         var q3Result = $("#question3").val();
+        var q3Result = $("#question4").val();
         var totalScore;
+        function car(answer) {
+            if (answer === "BMW") {
+                return 1;
+            }
+            else if (answer === "Chevy") {
+                return 2;
+            }
+        
+            else {
+                return 3;
+            }
+            
+        };
+        function party(answer) {
+            if (answer === "Yes"){
+                return 3;    
+            }
+            
+            else if (answer === "No"){
+                return 1;    
+            }
+            
+            else {
+                return 2;
+            }
+        }
+        function people(answer){
+            if (answer === "Yes"){
+                return 3;
+            }
+            else if (answer === "No"){
+                return 1;            
+            }
+            else {
+                return 2;
+            }
+         
+            
+        }
+        car(q1Result)
     });
 
 });
